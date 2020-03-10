1ca39d32dd6e851a81866815d18ec37ee9d29ba7
Rallpher <62035317+Rallpher@users.noreply.github.com>
2020-03-10 17:27:35 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,20 @@
+; <?php exit;?>
+
+[login]
+admin_user = "ralpher"
+admin_password = "a770b1eda7dc9c679a96ba0f6467db2e"
+
+[website]
+seo_urls = "0"
+currency = "$"
+date_format = "d/m/Y"
+results_per_page = "10"
+admin_email = "info@domain.com"
+use_captcha_images = "1"
+
+[email]
+smtp_server = "localhost"
+smtp_user = ""
+smtp_password = ""
+smtp_port = "25"
+
